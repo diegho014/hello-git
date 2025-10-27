c82ba14028a980362ef9c3ef3434c08b08129827
Diego <oncec672@gmail.com>
2025-10-27 16:50:59 -0500

    Directo en github

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,4 @@
 print(" Hello, GitHub!")
 print(" Hoy se implementaron unos nuevos cambios en el archivo ")
 print(" Nuevos cambios asosciados a la rama feature-a")
+print(" Cambios realizados directamente en GitHub ")
